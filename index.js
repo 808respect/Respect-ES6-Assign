dcde7a9a862afa746d399edde6d5310a04fe08f4
REESCOH <respect.r.tsvuura@gmail.com>
2023-05-08 10:46:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// QUESTION 1
+const PI = 3.14;
+console.log(PI);
+document.write(PI);
+
+// QUESTION 2
+var myName = "Respect";
+var age =  10 ;
+
+var myInfo = `My name is ${myName} and i am ${age} years old`;
+console.log(myInfo);
+
+// QUESTION 3
+var addition = (x, y) => console.log(`the sum of x and y is ${x + y}`);
+
+addition(3, 6);
+addition(3, 16);
+addition(3, -6);
+
+// QUESTION 4
+function sum(a, b) {
+    console.log(`the sum of a and b = ${a + b}`);
+}
+sum(100, 33);
+
+// QUESTION 5
+var name = "John";
+var greeting = console.log(`"Hello, ${name}!"`);
+
+ greeting();
